44239728ffaf7ade72747d90779ccdb6549b3e55
Eseosa Kay-Uwagboe Pascal <75255976+eseosapku@users.noreply.github.com>
2026-05-21 12:06:59 +0200

    materials

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MainMenu : MonoBehaviour
 {
-
+    private Color originalTrapColor;
+    private Color originalGoalColor;
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode; 
     public void QuitMaze()
     {
         Application.Quit();
@@ -14,12 +19,23 @@ public class MainMenu : MonoBehaviour
     }
     public void PlayMaze(int sceneID)
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = originalGoalColor;
+            goalMat.color = originalTrapColor;
+        }
         SceneManager.LoadScene(sceneID);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (trapMat != null) originalTrapColor = trapMat.color;
+        if (goalMat != null) originalGoalColor = goalMat.color;
     }
 
     // Update is called once per frame
